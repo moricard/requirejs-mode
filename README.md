c2e5e01de5b02e2d8ed2938a5a156c1e1a1c67a9
ricja <marco.ricard@gmail.com>
2013-02-10 18:29:13 -0500

    can create module placeholders

--- README.md
+++ README.md
@@ -1,4 +1,99 @@
 requirejs-mode
 ==============
 
-requireJS minore mode for emacs
+requireJS is a Backbone aware minor mode for emacs
+
+The goal is to make writing modules for [requireJS](http://requirejs.org) easier and more
+productive.
+
+## Installation
+
+Make sure you put the `requirejs-mode.el` in your load path.
+
+Then in your `init.el` file.
+
+```
+(require 'require-mode)
+;; replace 'js2-mode-hook with 'js-mode if you don't use js2-mode
+(add-hook 'js2-mode-hook
+    (lambda ()
+        (require-mode)))
+```
+
+## Dependencies
+
+We depend on `ido-mode` for the interactive import of a file as a dependency.
+
+## Keybindings
+
+* `C-c rf` : require-import file as module.
+* `C-c rn` : require-new-module.
+* `C-c rb` : require-new-backbone-module
+* `C-c r$` : require-new-jquery-module
+* `C-c rj` : require-new-jasmine-module
+
+## Usage
+
+### require-new-module
+
+As it is the one I use the most, this one defaults to `require-new-backbone-module`
+
+### require-new-backbone-module
+
+Generates:
+
+```
+define (
+    ['jquery'
+    ,'underscore'
+    ,'backbone'
+    ],
+    
+    function ( $, _, Backbone ) {
+
+    }
+);
+```
+
+### require-new-jquery-module
+
+Generates:
+
+```
+define (
+    ['jquery'
+    ],
+    
+    function ( $ ) {
+        
+    }
+);
+```
+
+### require-new-jasmine-module
+
+Generates:
+
+```
+define (
+    ['jasmine'
+    ],
+    
+    function ( Jasmine ) {
+        
+    }
+);
+```
+
+
+### require import file
+`M-x require-import`, or `C-c rf`
+
+Importing file as a module is *Backbone aware* in the sense that it will try to keep the
+subfolders of the imported files only if these are below `.../collections/`, `.../views/`,
+`.../models/`, `.../templates/`.
+
+For example, importing a file with the relative path `../collections/books.js` from a module
+will put `'collections/books'` in the dependencies list and `'Books'` in the function declaration.
+
+It will also prepend `text!` in the dependencies list item if the file loaded is a template.
--- requirejs-mode.el
+++ requirejs-mode.el
@@ -23,12 +23,53 @@
                             '(lambda (word) (capitalize (downcase word)))
                             (split-string s "-")) ""))
 
+(defun require-new-jquery-module ()
+  "initializes a jquery module"
+  (interactive)
+  (insert "define (
+    ['jquery'
+    ],
+    
+    function ( $ ) {
+        
+    }
+);")
+  (backward-char 9))
 
-(defun require-import ()
+;; insert a new requireJS module with backbon
+(defun require-new-backbone-module ()
+  "initializes an empty requireJS mnodule"
+  (interactive)
+  (insert "define (
+    ['jquery'
+    ,'underscore'
+    ,'backbone'
+    ],
+    
+    function ( $, _, Backbone ) {
+        
+    }
+);")
+  (backward-char 9))
+
+(defun require-new-jasmine-module ()
+  "inititializes an new jasmine require module"
+  (interactive)
+  (insert "define (
+    ['jasmine'
+    ],
+    
+    function ( Jasmine ) {
+        
+    }
+);")
+  (backward-char 9))
+
+(defun require-import (s)
   "add import to require header"
   (interactive)
-  (setq import (ido-read-file-name "Import RequireJS module: "))
-  (if (setq index (string-match "collections[/.]*[/]" import))
+
+ (if (setq index (string-match "collections[/.]*[/]" import))
       (setq import (substring import index))
     (if (setq index (string-match "models[/.]*[/]" import))
         (setq import (substring import index))
@@ -40,6 +81,8 @@
           (setq import (substring import index))))))
 
   (save-excursion 
+    (if (not (require-goto-headers))
+        (require-new-module))
     (require-goto-dependency-insert-point)
     (insert (concat
              ",'" (substring import 0 (string-match ".js$" import)) "'\n    "))
@@ -48,15 +91,32 @@
              ", " (camelize (substring import 
                                        (string-match "[a-z-]+[.]" import) 
                                        (string-match "[.]" import))))))
-  (message (concat "Adding " import " to dependencies."))
-)
+  (message (concat "Adding " import " to dependencies.")))
 
+(defun require-import-file ()
+  "add import to require header"
+  (interactive)
+  ; Get the file-name from interactive do.
+  (setq import (ido-read-file-name "Import RequireJS module: "))
+  (require-import import))
 
 (defvar require-mode-map (make-sparse-keymap)
   "require-mode keymap")
 
 (define-key require-mode-map
-  (kbd "C-c ri") 'require-import)
+  (kbd "C-c rf") 'require-import-file)
+
+(define-key require-mode-map
+  (kbd "C-c rn") 'require-new-backbone-module)
+
+(define-key require-mode-map
+  (kbd "C-d rb") 'require-new-backbone-module)
+
+(define-key require-mode-map
+  (kbd "C-c r$") 'require-new-jquery-module)
+
+(define-key require-mode-map
+  (kbd "C-c rj") 'require-new-jasmine-module)
 
 (define-minor-mode require-mode
   "Require mode" nil " requireJS" require-mode-map)
